d64c2d3ed36b70ef66a89ff13d6a2298d57a91bf
Antonis Zafeirelis <zaftony@hotmail.gr>
2021-06-06 01:21:35 +0300

    User Accounts Almost Ready
    Firebase implemented.
    Account Creation Completed.
    Argument Passing Broken for now.

--- CrowderApp/app/src/main/java/com/ceid/crowder/Registration.java
+++ CrowderApp/app/src/main/java/com/ceid/crowder/Registration.java
@@ -4,12 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Registration extends AppCompatActivity {
 
+    Registration(EditText email,EditText username,EditText password){
+        this.memail = email;
+        this.mpassword = password;
+        this.musername = username;
+    }
+
     private Button next;
+    EditText musername;
+    EditText memail;
+    EditText mpassword;
+    EditText mreppassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +31,63 @@ public class Registration extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_registration);
 
+        musername = findViewById(R.id.username);
+        memail = findViewById(R.id.regemail);
+        mpassword = findViewById(R.id.regpassword);
+        mreppassword = findViewById(R.id.password_rep);
+        Registration user = new Registration(memail,musername,mpassword);
+
         //Just The Next Button
         next = (Button) findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View V) {
+                //Collection and Checking of Data Inserted
+                String email = memail.getText().toString().trim();
+                String password = mpassword.getText().toString().trim();
+                String username = musername.getText().toString().trim();
+                String reppassword = mreppassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(username)){
+                    musername.setError("Παρακαλώ Εισάγετε Ένα Όνομα Χρήστη.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(email)){
+                    memail.setError("Παρακαλώ Εισάγετε Μία Διεύθυνση eMail.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mpassword.setError("Παρακαλώ Εισάγετε Έναν Κωδικό.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(reppassword)){
+                    mreppassword.setError("Παρακαλώ Εισάγετε Ξανά Τον Κωδικό.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    mpassword.setError("Ο Κωδικός Πρέπει να Περιέχει Τουλάχιστον 6 Ψηφία");
+                    return;
+                }
+
                 FinishRegistration();
             }
         });
+    }
+
+    public String getemail(){
+        return (memail.getText().toString().trim());
+    }
+
+    public String getusername(){
+        return (musername.getText().toString().trim());
+    }
+
+    public String getpass(){
+        return (mpassword.getText().toString().trim());
     }
 
     public void FinishRegistration() {
--- CrowderApp/app/src/main/java/com/ceid/crowder/RegistrationComplete.java
+++ CrowderApp/app/src/main/java/com/ceid/crowder/RegistrationComplete.java
@@ -1,26 +1,57 @@
 package com.ceid.crowder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationComplete extends AppCompatActivity {
 
     private CheckBox terms;
     private Button register;
     private boolean accepted = false;
+    private String email,username,password;
+
+    EditText mname,maddress,mpostcode,mcity;
+    ProgressBar registrationprogress;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        email = Registration.getemail();
+        username = Registration.getusername();
+        password = Registration.getpass();
+
         //Hide Title Bar Coz Crashes
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_registration_complete);
+
+        mname = findViewById(R.id.name);
+        maddress = findViewById(R.id.address);
+        mpostcode = findViewById(R.id.postCode);
+        mcity = findViewById(R.id.city);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        registrationprogress = findViewById(R.id.registrationprogress);
+
+        if(fAuth.getCurrentUser() != null){
+            GoToMain();
+        }
 
         //Terms of Service Checkbox
         terms = (CheckBox) findViewById(R.id.Terms);
@@ -36,9 +67,53 @@ public class RegistrationComplete extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View V) {
+
+                String name = mname.getText().toString().trim();
+                String address = maddress.getText().toString().trim();
+                String postcode = mpostcode.getText().toString().trim();
+                String city = mcity.getText().toString().trim();
+
+                if(TextUtils.isEmpty(address)){
+                    maddress.setError("Παρακαλώ Εισάγετε Μία Διεύθυνση.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(name)){
+                    mname.setError("Παρακαλώ Εισάγετε Το Ονοματεπώνυμο Σας.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(postcode)){
+                    mpostcode.setError("Παρακαλώ Εισάγετε Τον Ταχυδρομικό Σας Κώδικα.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(city)){
+                    mcity.setError("Παρακαλώ Εισάγετε Ένα Όνομα Χρήστη.");
+                    return;
+                }
+
                 //Check if Terms Accepted
                 if(accepted) {
-                    GoToMain();
+
+                    //Enable The Progress Bar
+                    registrationprogress.setVisibility(View.VISIBLE);
+
+                    //Register To Firebase
+                    fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegistrationComplete.this,"Η Εγγραφή Ήταν Επιτυχής!",Toast.LENGTH_SHORT).show();
+                                GoToLogin();
+                            }
+                            else{
+                                Toast.makeText(RegistrationComplete.this,"Η Εγγραφή Απέτυχε." + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
                 }
                 else{
                     terms.setError( "Πρέπει Να Αποδεχτείτε Τους Όρους Για Να Συνεχίσετε!" );
@@ -47,7 +122,12 @@ public class RegistrationComplete extends AppCompatActivity {
         });
     }
 
-    //Goes to Main
+    //Goes to Login
+    public void GoToLogin(){
+        Intent intent = new Intent(this, LoginFinal.class);
+        startActivity(intent);
+    }
+
     public void GoToMain(){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);

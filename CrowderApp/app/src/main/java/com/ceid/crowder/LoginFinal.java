2d1c8835972ffec417d2cb50c8367cffdef4c33d
Antonis Zafeirelis <zaftony@hotmail.gr>
2021-06-05 14:37:31 +0300

    Fixed Registration Screen

--- CrowderApp/app/src/main/java/com/ceid/crowder/LoginFinal.java
+++ CrowderApp/app/src/main/java/com/ceid/crowder/LoginFinal.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginFinal extends AppCompatActivity {
 
     private Button login;
+    private TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +24,24 @@ public class LoginFinal extends AppCompatActivity {
                 GoToMain();
             }
         });
+
+        register = (TextView) findViewById(R.id.notRegistered);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                GoToRegistration();
+            }
+        });
     }
 
      public void GoToMain(){
             Intent intent = new Intent(this, HomeActivity.class);
             startActivity(intent);
         }
+
+    public void GoToRegistration(){
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
+    }
 
     }
